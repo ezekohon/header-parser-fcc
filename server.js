ab5d4799ca3ef1437dc9cde3debd8126f0915726
Ezequiel <ezekohon@gmail.com>
2017-05-23 17:17:26 -0300

    finished

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,14 +19,19 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  let obj = {ipAdress:req.headers['host'], language:req.headers['accept-language'], software:req.headers['user-agent']};
+  res.json(obj); 
+});
 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+
+// listen for requests process.env.PORT:)
+var listener = app.listen(3222, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
